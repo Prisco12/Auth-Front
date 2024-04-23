306e92aac7da9d6dd156c831f2fd0656633867c4
Gabriel Prisco <gabrielmarcosprisco@gmail.com>
2024-04-23 17:14:26 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Form } from './pages/components/form'
 import './styles/global.css'
 import './App.css'
+import Home from './pages/Home'
 function App() {
 
 
   return (
-    <Form />
+    <Home/>
   )
 }
 
